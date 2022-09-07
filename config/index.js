e6c9b21c3b4bda01131d8fdc525beb13274b0c29
xqychenwang <82868200+xqychenwang@users.noreply.github.com>
2022-09-07 11:26:25 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -31,15 +31,15 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "彤彤",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
+      id: "opqes6apPjmYZ1tVMXjPg5DaZw2k",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "Wue80GrtY1nAi5a4tam4FDxZF59WNx9pwcBc06LMpANnu8",
+      useTemplateId: "cAr8zK4Lyu4w3BJ7DbqxcM-sCd4xYIMdKjcPdQG-yRo",
       // 所在省份
-      province: "广东",
+      province: "河南",
       // 所在城市
-      city: "肇庆",
+      city: "郑州",
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
       openUrl: "https://wangxinleo.cn"
     }, 
